35d2d360e4875786eb8f721516ec42c87f660a17
Blerand Gashi <blerandgashi15@gmail.com>
2025-07-22 00:36:45 +0200

    added some features in js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const userName = document.querySelector("#author");
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
   .then(response => response.json())
   .then(data => {
-    console.log(data.urls.raw);
     bodyImg.style.backgroundImage = `url('${data.urls.raw}')`
     userName.textContent = `By: ${data.user.name}`
   })
@@ -36,6 +35,29 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     console.log("The datas are not available, please come again soon!");
   })
 
+function getCurrentTime(){
+  const getHourData = new Date();
+  const currentTime = getHourData.toLocaleTimeString("en-us", {timeStyle: "short"});
+  document.querySelector(".time").textContent = currentTime
+}
+setInterval(getCurrentTime, 1000)
+
+navigator.geolocation.getCurrentPosition(position => {
+    console.log(position)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+      .then(response => {
+        if (!response.ok) {
+          throw Error("No datas found")
+        }
+        return response.json()
+      })
+      .then(data =>{
+        console.log(data);
+      })
+      .catch(err => {
+        console.log("datas not found" , err);
+      })
+});
 
 //.catch is used when the promise is rejected in .then blocks
 //.catch uses a photo I choosed after the promise is rejected ONLY if it was rejected
